3b8eb9826600c9f45562e6c8e30138e906473856
Gideon Bawa <kieriantirian@gmail.com>
2024-12-07 00:40:38 +0100

    Add files via upload

--- src/app/Hero/page.tsx
+++ src/app/Hero/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
 import { Download, Images } from 'lucide-react';
